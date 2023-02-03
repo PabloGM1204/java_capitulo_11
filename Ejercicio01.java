9cbe50346e9cd4fd895c0c64afad235a447db7d9
pabloGm1204 <pabloGm1204@gmail.com>
2023-02-03 16:02:51 +0100

    Ejercicio1

---
+++ Ejercicio01.java
@@ -0,0 +1,29 @@
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Ejercicio01{
+    public static void main(String[] args) {
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter("primos.dat"));
+            for(int i = 1; i<500; i++){
+                if(esPrimo(i)){
+                    bw.write(String.valueOf(i)+"\n");
+                }
+            }
+            bw.close();
+        } catch (IOException e) {
+            System.out.println("No se ha podido escribir en el fichero");
+        }
+    }
+
+    public static boolean esPrimo(int i){
+        for(int j = 2; j<=i/2; j++){
+            if(i%j == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
